26d0a78e88eb8c68006a281ccfd0ed761d15e397
pk85-sp3ct0r <git.pk85@sp3ct0r.one>
2023-01-07 22:12:06 +0700

    start dev

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,81 @@
-#include <iostream>
-//#include <HttpServer.h>
+#include "wfrest/HttpServer.h"
+#include "wfrest/json.hpp"
+#include "spdlog/spdlog.h"
+using namespace wfrest;
+using Json = nlohmann::json;
 
-using namespace std;
-
-int main(int argc, char** argv)
+int main()
 {
-    cout << "hello world!" << endl;
+    HttpServer svr;
+
+    svr.track([](HttpTask *server_task) {
+        HttpResp *resp = server_task->get_resp();
+        HttpReq *req = server_task->get_req();
+        HttpServerTask *task = static_cast<HttpServerTask *>(server_task);
+        Timestamp current_time = Timestamp::now();
+        std::string fmt_time = current_time.to_format_str();
+        spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] [WFREST] %v");
+        spdlog::info("[{}] {} | {} from {}:{}",
+                     resp->get_status_code(),
+                     req->get_method(),
+                     req->current_path().c_str(),
+                     task->peer_addr().c_str(),
+                     task->peer_port());
+    });
+
+    // curl -v http://ip:port/json1
+    svr.GET("/json1", [](const HttpReq *req, HttpResp *resp)
+    {
+        Json json;
+        json["test"] = 123;
+        json["json"] = "test json";
+        resp->Json(json);
+    });
+
+    // curl -v http://ip:port/json2
+    svr.GET("/json2", [](const HttpReq *req, HttpResp *resp)
+    {
+        std::string valid_text = R"(
+        {
+            "numbers": [1, 2, 3]
+        }
+        )";
+        resp->Json(valid_text);
+    });
+
+    // curl -v http://ip:port/json3
+    svr.GET("/json3", [](const HttpReq *req, HttpResp *resp)
+    {
+        std::string invalid_text = R"(
+        {
+            "strings": ["extra", "comma", ]
+        }
+        )";
+        resp->Json(invalid_text);
+    });
+
+    // recieve json
+    //   curl -X POST http://ip:port/json4
+    //   -H 'Content-Type: application/json'
+    //   -d '{"login":"my_login","password":"my_password"}'
+    svr.POST("/json4", [](const HttpReq *req, HttpResp *resp)
+    {
+        if (req->content_type() != APPLICATION_JSON)
+        {
+            resp->String("NOT APPLICATION_JSON");
+            return;
+        }
+        fprintf(stderr, "Json : %s", req->json().dump(4).c_str());
+    });
+
+    if (svr.start(8888) == 0)
+    {
+        getchar();
+        svr.stop();
+    } else
+    {
+        fprintf(stderr, "Cannot start server");
+        exit(1);
+    }
     return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -2,24 +2,24 @@ add_rules("mode.debug", "mode.release")
 set_project("A-Simple-Api-Cpp-wfrest")
 set_version("0.1.0")
 
+set_languages("c23", "c++23")
+
 package("wfrest")
     set_homepage("https://github.com/wfrest/wfrest")
 
-    set_kind("static")
     set_urls("https://github.com/wfrest/wfrest.git")
     add_urls("https://github.com/sogou/workflow/archive/refs/tags/$(version).tar.gz",
              "https://github.com/sogou/workflow/archive/refs/tags/$(version).zip",
              "https://github.com/wfrest/wfrest.git")
 
-    on_load(function (package)
-        print("Install Location:", package:installdir())
-        print("Cache Location:", package:cachedir())
-    end)
+    add_deps("workflow", "brew::spdlog")
+
 
     on_install("macosx", "linux", function (package)
-
-        import("package.tools.xmake").install(package)
-        print(import("package.tools.xmake").buildenvs(package))
+        local configs = {kind = "static", plat = os.host(), arch = os.arch()}
+        local options = {wfrest_lib = package:installdir("lib"), wfrest_inc = package:installdir("include")}
+        import("package.tools.xmake").install(package, configs, options)
+        --print(import("package.tools.xmake").buildenvs(package))
         --os.cp("libwfrest.a", package:installdir("lib"))
     end)
 
@@ -29,5 +29,5 @@ add_requires("wfrest main", {alias = "wfrest"})
 
 target("A-Simple-Api-Cpp-wfrest")
     set_kind("binary")
-    add_files("src/*.cpp")
     add_packages("wfrest")
+    add_files("src/*.cpp")
